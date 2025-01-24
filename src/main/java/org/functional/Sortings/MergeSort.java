c284d2fb17617a052b79fcfb8de0f3cd2cb32e2a
Shivam Sharma <73551425+Shivam5197@users.noreply.github.com>
2025-01-24 13:12:51 +0530

    DSA for 24 JAN  2025

--- src/main/java/org/functional/Sortings/MergeSort.java
+++ src/main/java/org/functional/Sortings/MergeSort.java
@@ -84,7 +84,6 @@ public class MergeSort {
             left++;
         }
 
-
         // Copy the remaining elements from the right half, if any
         while (right <= lastIndex) {
             temp.add(arr[right]);
